eda333598f6625416fd729bb262686217f276e4a
Lakshan Rajapaksha <lrajapaksha58@gmail.com>
2023-03-15 08:50:49 +0530

    create login api

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Notifications\MessageSent;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -13,20 +14,26 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    protected $table ="users";
-    protected $quarded =['id'];
-   
-    
+    protected $table = "users";
+    protected $guarded = ['id'];
 
     protected $hidden = [
         'password',
-       
     ];
 
-   const USER_TOKEN="userToken";
+    const USER_TOKEN = "userToken";
 
-   public function chats(): HasMany
-   {
-    return $this->hasMany(Chat::class,'created_by');
-   }
+    public function chats(): HasMany
+    {
+        return $this->hasMany(Chat::class, 'created_by');
+    }
+
+    public function routeNotificationForOneSignal() : array{
+        return ['tags'=>['key'=>'userId','relation'=>'=', 'value'=>(string)($this->id)]];
+    }
+
+    // public function sendNewMessageNotification(array $data) : void {
+    //     $this->notify(new MessageSent($data));
+    // }
+
 }
